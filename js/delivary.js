ed65f13794d532aaf23f1fcb9070d5eb7716684d
prabuddhafernando <m.l.prabuddhafernando619@gmail.com>
2017-03-14 10:22:56 +0530

    added every ui

---
+++ js/delivary.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Prabudda on 3/13/2017.
+ */
+
+$(document).on('click', '.panel-heading span.clickable', function(e){
+    var $this = $(this);
+    if(!$this.hasClass('panel-collapsed')) {
+        $this.parents('.panel').find('.panel-body').slideUp();
+        $this.addClass('panel-collapsed');
+        $this.find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+    } else {
+        $this.parents('.panel').find('.panel-body').slideDown();
+        $this.removeClass('panel-collapsed');
+        $this.find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+    }
+})
